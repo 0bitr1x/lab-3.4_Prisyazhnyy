aa495bde98b79b35a25cdf07df4dd6b1ff35d731
prisy <prisy@LAPTOP-23R98U4U>
2023-10-30 01:19:07 +0400

    Добавьте файлы проекта.

---
+++ lab№3.4_Prisyazhnyy/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите верхний предел сигмы k: ");
+double kMax = double.Parse(Console.ReadLine());
+Console.WriteLine("|   x   |  f (x)  |");
+double sum = 0;
+for (double x = 1; x <= 2.1; x += 0.1)
+{
+    for(double k = 1; k <= kMax; k++)
+    {
+        double factorial = 1;
+        double f = 2 * k - 1;
+        for(double i = 1; i <= f; i++) factorial *= i;
+        sum += (Math.Exp(k) * Math.Pow(x, k)) / factorial;
+    }
+    Console.WriteLine($"|{x,5:F2}{"|",3}{sum,7:F2}{"|",3}");
+}
+
